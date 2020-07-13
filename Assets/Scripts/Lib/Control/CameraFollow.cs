8e0287eb03b0f2255c2987202bfee96017cdcefb
ZhangQichao1988 <tyoukeityou@yahoo.co.jp>
2020-07-13 22:04:52 +0800

    添加岩石模型，添加技能图标

--- Assets/Scripts/Lib/Control/CameraFollow.cs
+++ Assets/Scripts/Lib/Control/CameraFollow.cs
@@ -102,7 +102,7 @@ public class CameraFollow : MonoBehaviour
 			}
 
 			Distance = Mathf.Lerp(55f, 90f, currentRate);
-			FixOffsetPos.z = Mathf.Lerp(-5f, -13f, currentRate);
+			//FixOffsetPos.z = Mathf.Lerp(-5f, -13f, currentRate);
 			SelfCamera.nearClipPlane = Mathf.Lerp(40f, 70f, currentRate);
 			SelfCamera.farClipPlane = Mathf.Lerp(80f, 150f, currentRate);
 
