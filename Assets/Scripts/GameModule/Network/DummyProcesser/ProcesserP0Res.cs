30e51d4ddc3aca2a054a442d61a4da782d3c4407
li-j <li-j@klab.com>
2020-08-12 12:11:46 +0800

    重构dummydata p6 add

--- Assets/Scripts/GameModule/Network/DummyProcesser/ProcesserP0Res.cs
+++ Assets/Scripts/GameModule/Network/DummyProcesser/ProcesserP0Res.cs
@@ -5,21 +5,15 @@ using UnityEngine;
 /// <summary>
 /// 注册 
 /// </summary>
-public class ProcesserP0Res : IDummyResponseProcesser
+public class ProcesserP0Res : BaseDummyProcesser<P0_Request, P0_Response>
 {
-    public IMessage ProcessRequest(int msgId, IMessage pbData)
+    public override P0_Response ProcessRequest(int msgId, P0_Request pbData)
     {
-        var request = pbData as P0_Request;
         var response = GetResponseData();
         PlayerPrefs.SetString(NetworkConst.AUTH_KEY, response.AuthToken);
         PlayerPrefs.Save();
         
         return response;
-    } 
-
-    public void DispatchRes(int msgId, IMessage request, IMessage response)
-    {
-        NetWorkHandler.GetDispatch().Dispatch<P0_Response>(NetWorkHandler.GetDispatchKey(msgId), response as P0_Response);
     }
 
     P0_Response GetResponseData()
