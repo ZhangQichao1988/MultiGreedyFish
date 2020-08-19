ac98528c613b4c5fceb1ee3b57f37eb6e77a16db
ZhangQichao1988 <tyoukeityou@yahoo.co.jp>
2020-08-19 15:58:37 +0800

    鱼选择界面鱼旋转操作对应

--- Assets/Scripts/GameModule/UI/Home/FishStatusFishControl.cs
+++ Assets/Scripts/GameModule/UI/Home/FishStatusFishControl.cs
@@ -7,41 +7,63 @@ public class FishStatusFishControl : MonoBehaviour
 {
 
     public Camera camera;
-    float offset = 5f;
-    float spd = 5f;
-    float animSpd = 0.025f;
-    float changePosLen = 0.1f;
-    //GameObject effectBlister;
     Animator animator;
     Transform transModel;
-    Vector3 Dir, curDir, moveDir;
-    Vector3 targetPos;
-    Vector3 moveVec;
+
+    float FishRotAngle = 0f;
+    float preTouchPosX = 0f;
+    float nowTouchPosX = 0f;
+    bool isDrage = false;
 
     public void SetFishModel(GameObject goFish)
     {
         if (transModel != null) { Destroy(transModel.gameObject); }
         transModel = goFish.transform;
-        targetPos = transModel.position;
         animator = goFish.GetComponent<Animator>();
-        GameObject goMouth = GameObject.Find("mouth");
-        Debug.Assert(goMouth != null, "HomeFishControl.SetFishModel()_1");
-        GameObject go = ResourceManager.LoadSync(Path.Combine(AssetPathConst.effectRootPath, "fx_blister_home"), typeof(GameObject)).Asset as GameObject;
-        Instantiate(go, goMouth.transform);
+        animator.SetFloat("Speed", 1f);
+        //GameObject goMouth = GameObject.Find("mouth");
+        //Debug.Assert(goMouth != null, "HomeFishControl.SetFishModel()_1");
+        //GameObject go = ResourceManager.LoadSync(Path.Combine(AssetPathConst.effectRootPath, "fx_blister_home"), typeof(GameObject)).Asset as GameObject;
+        //Instantiate(go, goMouth.transform);
     }
     private void Update()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Ray camRay = camera.ScreenPointToRay(Input.mousePosition);
-        //    RaycastHit floorHit;
-        //    int layMask = 1 << LayerMask.NameToLayer("HomeCollider");
-        //    if (Physics.Raycast(camRay, out floorHit, 5000f , layMask))
-        //    {
-        //        targetPos = floorHit.point;
-        //        spd = 5f;
-        //    }
-        //}
+        if (Input.GetMouseButton(0))
+        {
+            if (!isDrage)
+            {
+                Ray camRay = camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                int layMask = 1 << LayerMask.NameToLayer("Fish");
+                if (Physics.Raycast(camRay, out hit, 5000f, layMask))
+                {
+                    isDrage = true;
+                }
+            }
+
+            if (isDrage)
+            {
+                nowTouchPosX = Input.mousePosition.x;
+                if (preTouchPosX != 0f)
+                {
+                    FishRotAngle += (preTouchPosX - nowTouchPosX) * 0.5f;
+                    transModel.rotation = Quaternion.AngleAxis(FishRotAngle, Vector3.up);
+                }
+                preTouchPosX = nowTouchPosX;
+            }
+        }
+        else 
+        {
+            isDrage = false;
+            preTouchPosX = 0f;
+        }
+
+
+        
+
+
+
+
         //moveVec = new Vector3(targetPos.x, targetPos.y, targetPos .z - offset) - transModel.position;
         //Dir = moveVec.normalized;
         //// 靠近目的地后就开始闲逛
