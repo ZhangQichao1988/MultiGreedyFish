7696b53854276312fe225fb5044058e6a22a8c77
ZhangQichao1988 <tyoukeityou@yahoo.co.jp>
2020-11-28 21:16:20 +0800

    遥感灵敏度提高

--- Assets/Scripts/Lib/Control/InGameUIPanel.cs
+++ Assets/Scripts/Lib/Control/InGameUIPanel.cs
@@ -16,7 +16,7 @@ public class InGameUIPanel : MonoBehaviour
     public Button skillBtn;
     private PlayerBase Player;
     private RectTransform SelfRectTF;
-    private float MaxLength = 120;
+    private float MaxLength = 60;
     // Start is called before the first frame update
     void Start()
     {
