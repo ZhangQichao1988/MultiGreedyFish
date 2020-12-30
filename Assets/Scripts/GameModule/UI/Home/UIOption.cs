18c6a2d098a112c47439dc12c8096c50863f0a25
ZhangQichao1988 <tyoukeityou@yahoo.co.jp>
2020-12-30 16:21:14 +0800

    设置界面调整

--- Assets/Scripts/GameModule/UI/Home/UIOption.cs
+++ Assets/Scripts/GameModule/UI/Home/UIOption.cs
@@ -24,10 +24,16 @@ public class UIOption : UIBase
     };
     public Dropdown languageSelect;
     public AudioMixer audioMixer;
+    public Text textPlayerID;
+    public Text textNickName;
+
+    string setNickName;
 
     public override void Init()
     {
         base.Init();
+        textPlayerID.text = PlayerModel.Instance.playerId.ToString();
+        textNickName.text = PlayerModel.Instance.player.Nickname;
         int languageModeIndex = PlayerPrefs.GetInt(AppConst.PlayerPrefabsOptionLangauge, (int)AppConst.languageMode);
         foreach (var note in languageModes)
         {
@@ -35,17 +41,48 @@ public class UIOption : UIBase
         }
         languageSelect.SetValueWithoutNotify(languageModeIndex);
     }
-    public void Drop_LangaugeSelect(int n)
+    public void SelectLangauge(int n)
     {
         AppConst.languageMode = languageModes[n].languageMode;
         PlayerPrefs.SetInt(AppConst.PlayerPrefabsOptionLangauge, n);
     }
-    public void Drop_BgmValue(float n)
+    public void SetBgmValue(float n)
     {
         audioMixer.SetFloat("BgmValue", n);
     }
-    public void Drop_SeValue(float n)
+    public void SetSeValue(float n)
     {
         audioMixer.SetFloat("SeValue", n);
+    }
+    // 通知开关
+    public void SetNoticeEnable(bool n)
+    {
+        Debug.Log(n);
+        // TODO:通知开关设定
+    }
+    // 联系客服
+    public void SendSupportMail()
+    {
+        Debug.Log("联系客服");
+        // TODO:联系客服
+    }
+    // 服务规约
+    public void OpenServiceAgreement()
+    {
+        Application.OpenURL(LanguageDataTableProxy.GetText(320));
+    }
+    // 设置昵称
+    public void SetNickName(string value)
+    {
+        setNickName = value;
+        NetWorkHandler.GetDispatch().AddListener<P9_Response>(GameEvent.RECIEVE_P9_RESPONSE, OnRecvChangeNickName);
+        NetWorkHandler.RequestModifyNick(setNickName);
+
+    }
+    void OnRecvChangeNickName<T>(T response)
+    {
+        NetWorkHandler.GetDispatch().RemoveListener(GameEvent.RECIEVE_P9_RESPONSE);
+        var res = response as P9_Response;
+        PlayerModel.Instance.player.Nickname = setNickName;
     }
 }
