00b336b25696d999575b49558ee57be6467cb3c1
li-j <li-j@klab.com>
2021-01-23 14:21:42 +0800

    update response

--- Assets/Scripts/GameModule/Constant/NetWorkResponseCode.cs
+++ Assets/Scripts/GameModule/Constant/NetWorkResponseCode.cs
@@ -8,4 +8,15 @@ public class NetWorkResponseCode
     public static int NO_ENOUGH_DIAMOND = 6;
     public static int NO_ENOUGH_GOLD = 7;
     public static int GOOD_POOL_MAX_LEVEL = 8;
+
+    //排行相关
+    
+    //请求的排行榜不存在
+    public static int RANK_BOARD_NO_EXIST = 9;
+
+    //没有当前玩家的排行
+    public static int USER_NO_RANK = 10;
+
+    //排行榜数据为空
+    public static int NO_RANK_DATA = 11;
 }
